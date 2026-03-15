fc7b9384c44cfa4646fb4d09ec501c6e4657b6ae
sikelelwemadula <sikemadula@gmail.com>
2026-03-15 06:34:02 +0200

    Added modal styling

--- scripts.js
+++ scripts.js
@@ -1,28 +1,5 @@
 import { initialTasks } from "./initialData.js"; 
 
-// DOM Elements
-//function createTaskElement(task) {
-//  const taskDiv = document.createElement("div");
-  //taskDiv.className = "task-div";
- // taskDiv.textContent = task.title;
- // taskDiv.dataset.taskId = task.id; // Store the task ID in a data attribute
-
- // const modal = document.getElementById('task-modal');
-
-  //taskDiv.addEventListener("click", () => { 
-  //If open, close it; otherwise, open it with the task details
-  //if (modal.open) {
-   //   modal.close();
- //   } else {
-      // Pass task data to fill the modal before opening
- //     openTaskModal(task); 
- //     modal.showModal(); // Or modal.show()
- //   }
- // });
-
-//  return taskDiv;
-//}
-
 // Get modal elements
 const modal = document.getElementById('task-modal');
 const closeBtn = document.getElementById('close-modal-btn');
